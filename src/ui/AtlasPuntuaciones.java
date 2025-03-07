70b25444fb6401b8b9abf3d79dd95c249c3dea5f
Natalia Tovar Diaz <1114953769@u.icesi.edu.co>
2025-03-07 12:58:35 -0500

    Enviando tarea de Natalia

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -14,6 +14,10 @@ public class AtlasPuntuaciones {
 	// Declaración del Escaner que nos ayudará en todo el programa
 	private Scanner escaner;
 
+	static final int CANTIDAD_JUGADORES = 10;
+	static final int CANTIDAD_RONDAS = 5;
+
+	
 	// Declaración de nuestras constantes a utilizar
 
 
@@ -24,6 +28,50 @@ public class AtlasPuntuaciones {
 	
 	public void run()
 	{
+	boolean flag = true;
+	int cantidad_jugadores = 0;
+	while (flag) {
+		System.out.println("Ingrese la cantidad de jugadores (Maximo 10)");
+		cantidad_jugadores = escaner.nextInt();
+		flag = comprobarMaximo(cantidad_jugadores, CANTIDAD_JUGADORES);
+		}
+	
+
+	flag = true;
+	int cantidad_rondas = 0;
+	while (flag){
+	System.out.println("Ingrese el numero de rondas");
+		cantidad_rondas[i] = escaner.nextInt();
+		flag = atlas.comprobarMaximo(cantidad_rondas,CANTIDAD_RONDAS);
+	}
+
+	
+
+	//Arreglo de los puntajes de los jugadores
+	int [] jugadores = new int[cantidad_jugadores];
+	double[] promJugadores = new double[cantidad_Jugadores];
+
+	atlas.entradaDePuntajes(cantidad_jugadores,cantidad_rondas,jugadores);
+	atlas.promedio(promJugadores, jugadores);
+	System.out.println("Resultados del torneo:");
+	int jugadorPuntuacionMasAlta = atlas.mostrarResultados(jugadores, promjugaores);
+	System.out.println("El jugador con la puntuacion mas alta es: Jugador " + jugadorPuntuacionMasAlta);
+
+
+	
+	
+		int cantidad_jugadores;
+		int[] numeroderondas = new int[CANTIDAD_RONDAS];
+		int[] puntaje = new int[CANTIDAD_RONDAS];
+
+		for(int i = 0; i < CANTIDAD_JUGADORES; i++){
+		System.out.println("Ingrese la cantidad de jugadores");
+		cantidad_jugadores[i] = escaner.nextInt();
+		}
+
+		for(int i = 0; i < CANTIDAD_RONDAS; i++){
+		System.out.println("Ingrese el numero de rondas");
+		cantidad_rondas[i] = escaner.nextInt();
 		// Declaracion de todas las variables a usar en el programa
 
 
@@ -40,6 +88,7 @@ public class AtlasPuntuaciones {
 
 
 	}
+	}
 
 	public static void main(String[] args) {
 		AtlasPuntuaciones mainApp = new AtlasPuntuaciones();
@@ -47,8 +96,11 @@ public class AtlasPuntuaciones {
 	}
 
 	/**
-	 * Descripción: El método calcularSumaTotal permite ...
-	 * @param int[] numeros
+	 * Descripción: El método calcularSumaTotal permite sumar y guardar los puntajes de los jugadores
+	 * @param int cantidadJugadores
+	 * @param int cantidadRondas
+	 * @param int[] jugadores
+	 * 
 	 * @return 
 	 */
     public int calcularSumaTotal(int[] numeros) {
@@ -72,8 +124,6 @@ public class AtlasPuntuaciones {
     public double encontrarMayor(double[] numeros) {
         // Completar
     }
-
-	
 
 	
 }
